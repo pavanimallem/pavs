a4dbd982f7cfad1e0e248ad8bf8086c0be0ce2c0
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2018-08-10 21:16:23 +0530

    Create guvi20.py

---
+++ guvi20.py
@@ -0,0 +1,11 @@
+def main ():
+num=int(input("display the multiplicant numbers"))
+         for i in range(1,5):
+	           	print (num,'x',i,"=",num*i)
+
+if_name_ = " _main_"
+      main()
+		
+
+	
+	
