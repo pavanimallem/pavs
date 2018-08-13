57b3bcfa18da2b1b6d216abfede85579fadbe439
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2018-08-13 13:09:14 +0530

    Update guvi21.py

--- guvi21.py
+++ guvi21.py
@@ -5,7 +5,7 @@ def arithmetic_progression():
 
 	n = float(input[0])
 	a = float(input[1])
-	a = float(input[2])
+	d = float(input[2])
 
 	s = ( ( a * 2 ) + ( ( n - 1 ) * d ) ) * ( n / 2 )
 	print( 'sum to n terms of given AP series: ' +str(s))
