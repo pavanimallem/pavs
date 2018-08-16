04af7e0af954fdc301b13ea30cd3255e2b63b995
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2018-08-16 15:08:49 +0530

    Update guvi30.py

--- guvi30.py
+++ guvi30.py
@@ -1,7 +1,7 @@
 h1,m1=map(int,raw_input().split())
 h2,m2=map(int,raw_input().split())
-x1=hr1*60+min1
-x2=hr2*60+min2
+x1=h1*60+m1
+x2=h2*60+m2
 diff=abs(x1-x2)
 time=diff%60
 time1=(diff-time)//60
