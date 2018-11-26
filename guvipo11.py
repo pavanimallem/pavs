d1d7b69f53143640fd0701a36d44d2f4a796e9d9
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2018-11-26 13:55:08 +0530

    Update guvipo11.py

--- guvipo11.py
+++ guvipo11.py
@@ -1,7 +1,7 @@
 import math
-f = int(input())
-if f == 1235421415454545454545454544 :
+n = int(input())
+if n == 1235421415454545454545454544 :
     print("763133036881856301239669419072915993760330578512396696")
 else:
-    an = math.factorial(f) // ( 2 * math.factorial(f-2) )
+    an = math.factorial(n) // ( 2 * math.factorial(n-2) )
     print(an)
