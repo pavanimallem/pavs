4c577e343d380b00e4986628cc145b50cafc5dc6
pavanimallem <41769826+pavanimallem@users.noreply.github.com>
2018-12-23 21:30:50 +0530

    Update guvip21.py

--- guvip21.py
+++ guvip21.py
@@ -2,6 +2,6 @@ x1,y1=map(int,raw_input().split())
 x2,y2=map(int,raw_input().split())
 x3,y3=map(int,raw_input().split())
 if ((x1*(y1-y2)+x2*(y2-y3)+x3*(y3-y1))==0):
-    print ("Yes")
+    print ("yes")
 else:
-    print ("No")
+    print ("no")
